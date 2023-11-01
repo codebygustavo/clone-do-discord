852afaace23123a63db26b4129724cdc07374e76
codebygustavo <hbw3.gustavo@gmail.com>
2023-11-01 14:20:46 -0300

    Folders setup

---
+++ app/(auth)/layout.tsx
@@ -0,0 +1,10 @@
+const AuthLayout = ({ childrean }: { childrean: React.ReactNode }) => {
+    return (
+        <div className="bg-red-500 h-full">
+            {childrean}
+        </div>
+    );
+}
+
+export default AuthLayo
+ut;
